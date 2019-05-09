ca520924de22ec9e02d338d9d9b05e4e39041085
mdzafar.i <mdzafar.i@hcl.com>
2019-05-09 16:31:54 +0530

    yahoo weather api uses.

--- app/src/main/java/com/learn/MainActivity.kt
+++ app/src/main/java/com/learn/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.learn.caculator.CalculatorActivity
+import com.learn.sunset.SunsetActivity
 import com.learn.tictactoe.R
 import com.learn.tictactoe.TicTacToeActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -19,6 +20,10 @@ class MainActivity : AppCompatActivity() {
         }
         btnCalculator.setOnClickListener {
             startActivity(Intent(this, CalculatorActivity::class.java))
+        }
+
+        btnGetSunset.setOnClickListener {
+            startActivity(Intent(this, SunsetActivity::class.java))
         }
     }
 }
